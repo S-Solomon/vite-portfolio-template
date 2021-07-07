bbc5953832ae392780ee1330b1d7e41b5cef9bf7
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-07-07 15:47:06 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css'
 import Cover from './components/cover/Cover' ;
+import Navbar from './components/navbar/Navbar'
 
 function App() {
+  const [scrollHeight, setScrollHeight] = useState(0);
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollHeight(position);
+    console.log(position);
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  },[scrollHeight])
+  
 
   return (
     <div className="App">
+      <Navbar isScrolling={scrollHeight}/>
       <Cover />
     </div>
   )
