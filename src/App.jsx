f3886fed35b78978916047fe9e12ba9c0a0aa09e
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-07-08 07:50:55 +0100

    info component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Cover from './components/cover/Cover' ;
 import Navbar from './components/navbar/Navbar' ;
 import About from './components/about/About' ;
 import Slider from './components/slider/Slider' ;
+import Info from './components/info/Info' ;
 
 function App() {
   const [scrollHeight, setScrollHeight] = useState(0);
@@ -24,6 +25,7 @@ function App() {
       <Cover />
       <About />
       <Slider />
+      <Info />
     </div>
   );
 }
