dfd5921fa9239ec48b822001c3bd243e9888648f
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-07-13 18:28:48 +0100

    err

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react' ;
 import './About.css' ;
-import removeBg from '../../assets/removebg.png' ;
+import removeBg from '../../assets/removeBg.png' ;
 
 const About = () => {
     return (
